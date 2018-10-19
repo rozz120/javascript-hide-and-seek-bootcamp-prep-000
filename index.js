e0b65fe2ddf17376eeb02d5438d99126d744b334
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-10-19 09:48:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
 
+function nestedTarget()
+{
+  return document.getElementById("nested").querySelector("div.target")
+}
+function increaseRankBy(n)
+{
+  const list = document.getElementByID("app").querySelectorAll("ul.ranked-lists");
+  var i;
+  for(i-0; i< list.length; i++)
+  {
+    list[i].innerHTML = ((parseInt(list[i].innerHTML))+ n);
+  }
+}
+
+function deepestChild()
+{
+  var deep = document.getElementByID("grand-node").querySelectorAll("div")
+  return deep[deep.length-1]
+}
